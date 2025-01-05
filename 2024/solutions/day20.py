ccc2e012e424f4a41d56651c6646531ad5d04653
Thomas Peyró <thomas.voeffray@hotmail.com>
2025-01-05 17:23:01 +0100

    Simplify solution to day 20 part 1 2024

--- 2024/solutions/day20.py
+++ 2024/solutions/day20.py
@@ -35,22 +35,21 @@ def find_path(racetrack):
 
 
 def day20_part1(racetrack, diff):
+    # There is exactly one path (verified on the inputs)
     path = find_path(racetrack)
+    # Approach: find all "shortcuts" within the path
+    # `diff` is the minimum time saved
     count_cheats = 0
     for i in range(len(path)-diff):
         point_a = path[i]
         for j in range(i+1, len(path)):
             point_b = path[j]
             # Check that the two points are separated by a Manhattan distance of 2
-            if abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1]) == 2:
-                manhattan_neighbors = lambda x, y: [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
-                coords = set(manhattan_neighbors(point_a[0], point_a[1])).intersection(manhattan_neighbors(point_b[0], point_b[1]))
-                for coord in coords:
-                    if racetrack[coord[0]][coord[1]] == "#":
-                        time_saved = j - i - 2
-                        # print(coord, point_a, point_b, i, j, time_saved)
-                        if time_saved >= diff:
-                            count_cheats += 1
+            manhattan_distance = abs(point_a[0] - point_b[0]) + abs(point_a[1] - point_b[1])
+            if manhattan_distance == 2:
+                time_saved = j - i - manhattan_distance
+                if time_saved >= diff:
+                    count_cheats += 1
     return count_cheats
 
 racetrack_example = """
